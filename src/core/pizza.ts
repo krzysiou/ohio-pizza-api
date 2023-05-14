0d964e0b208354fac4221be4264badaa0f4794bf
Matiixx <87618425+Matiixx@users.noreply.github.com>
2023-05-14 16:17:36 +0200

    feat: test usage of db client

--- src/core/pizza.ts
+++ src/core/pizza.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { PostgresClient } from '../db';
 
 type Pizza = {
   name: string;
@@ -32,8 +33,10 @@ const pizzaJSON: Pizza[] = [
   },
 ];
 
-const getPizzas = (_req: Request, res: Response<Pizza[]>) => {
+const getPizzas = async (_req: Request, res: Response<Pizza[]>) => {
   // TODO: get data from database then remove mock json data
+  // const pgRes = await PostgresClient.query<Pizza[]>('SELECT * FROM pizza');
+
   return res.json(pizzaJSON).status(200).send();
 };
 
