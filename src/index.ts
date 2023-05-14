667a6ebd89eb837a5ea8719f05a8f3ff1ad59607
Matiixx <87618425+Matiixx@users.noreply.github.com>
2023-05-14 16:16:53 +0200

    feat: init connection to db

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import express, { Express } from 'express';
 import { bindings } from './bindings';
 import { initRouter } from './utils/init-router';
+import { initDBConnection } from './db';
 
 dotenv.config();
 
@@ -11,4 +12,5 @@ const port = process.env.PORT;
 
 app.use(cors({ origin: 'http://localhost:3000', methods: ['GET', 'POST'] }));
 
+initDBConnection();
 initRouter(app, port, bindings);
