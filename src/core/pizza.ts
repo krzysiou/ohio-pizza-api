e196a2b0650cb31fe09dbf0bbf2c0bab7e1bcaf2
Matiixx <87618425+Matiixx@users.noreply.github.com>
2023-05-25 19:52:14 +0200

    chore: remove mock data

--- src/core/pizza.ts
+++ src/core/pizza.ts
@@ -9,31 +9,6 @@ type Pizza = {
   pic_url?: string;
 };
 
-const pizzaJSON: Pizza[] = [
-  {
-    name: 'Margherita',
-    ingredients: ['tomato sauce', 'mozzarella cheese', 'fresh basil'],
-    price: 9.99,
-  },
-  {
-    name: 'Pepperoni',
-    ingredients: ['tomato sauce', 'mozzarella cheese', 'pepperoni'],
-    price: 11.99,
-  },
-  {
-    name: 'Vegetarian',
-    ingredients: [
-      'tomato sauce',
-      'mozzarella cheese',
-      'mushrooms',
-      'green peppers',
-      'red onions',
-      'black olives',
-    ],
-    price: 10.99,
-  },
-];
-
 const getPizzas = async (_req: Request, res: Response<Pizza[]>) => {
   // TODO: get data from database then remove mock json data
   // const pgRes = await PostgresClient.query<Pizza[]>('SELECT * FROM pizza');
