ce75be7754354a42c9644c4f3bc1e5cfc82a4e0c
Matiixx <87618425+Matiixx@users.noreply.github.com>
2023-06-25 16:22:01 +0200

    feat: create register tests

--- src/tests/endpoints.test.ts
+++ src/tests/endpoints.test.ts
@@ -62,6 +62,41 @@ describe('test endpoints', () => {
       expect(res.data.accessToken).toEqual(expect.any(String));
     });
   });
+
+  describe('test register', () => {
+    it('should return 400 if we dont pass any data', async () => {
+      await axios
+        .post('http://localhost:3001/admin/register', {})
+        .catch((err) => {
+          expect(err.response.status).toBe(400);
+          expect(err.response.data.message).toEqual(
+            'Username or password was not provided'
+          );
+        });
+    });
+
+    it('should return 400 if username already exists', async () => {
+      await axios
+        .post('http://localhost:3001/admin/register', {
+          username: 'admin1',
+          password: 'admin1',
+        })
+        .catch((err) => {
+          expect(err.response.status).toBe(400);
+          expect(err.response.data.message).toEqual('Username already taken');
+        });
+    });
+
+    it('should return 200 if user was created', async () => {
+      const res = await axios.post('http://localhost:3001/admin/register', {
+        username: randomString(10),
+        password: randomString(10),
+      });
+      expect(res.status).toBe(200);
+      expect(res.data).toHaveProperty('accessToken');
+      expect(res.data.accessToken).toEqual(expect.any(String));
+    });
+  });
 });
 
 const randomString = (n: number) =>
