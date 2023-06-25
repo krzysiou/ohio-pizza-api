bf86147c865120e0c4c796830e0e36c0bcbbdcf2
Matiixx <87618425+Matiixx@users.noreply.github.com>
2023-06-25 17:04:26 +0200

    refactor: add test describe

--- src/tests/endpoints.test.ts
+++ src/tests/endpoints.test.ts
@@ -2,29 +2,31 @@
 import axios from 'axios';
 
 describe('test endpoints', () => {
-  it('should return correct pizzas array', async () => {
-    const res = await axios.get('http://localhost:3001/pizza');
+  describe('test get pizzas', () => {
+    it('should return correct pizzas array', async () => {
+      const res = await axios.get('http://localhost:3001/pizza');
 
-    expect(res.status).toBe(200);
-    expect(res.data).toBeInstanceOf(Array);
-    expect(res.data.length).toBeGreaterThan(0);
+      expect(res.status).toBe(200);
+      expect(res.data).toBeInstanceOf(Array);
+      expect(res.data.length).toBeGreaterThan(0);
 
-    res.data.every((pizza: any) => {
-      expect(pizza).toHaveProperty('pizza_id');
-      expect(pizza.pizza_id).toEqual(expect.any(Number));
-      expect(pizza).toHaveProperty('name');
-      expect(pizza.name).toEqual(expect.any(String));
-      expect(pizza).toHaveProperty('ingredients');
-      expect(pizza.ingredients).toBeInstanceOf(Array);
-      expect(
-        pizza.ingredients.every((i: unknown) => typeof i === 'string')
-      ).toBe(true);
-      expect(pizza).toHaveProperty('price');
-      expect(pizza.price).toEqual(expect.any(Number));
-      expect(pizza).toHaveProperty('pic_url');
-      expect(typeof pizza.pic_url === 'string' || pizza.pic_url === null).toBe(
-        true
-      );
+      res.data.every((pizza: any) => {
+        expect(pizza).toHaveProperty('pizza_id');
+        expect(pizza.pizza_id).toEqual(expect.any(Number));
+        expect(pizza).toHaveProperty('name');
+        expect(pizza.name).toEqual(expect.any(String));
+        expect(pizza).toHaveProperty('ingredients');
+        expect(pizza.ingredients).toBeInstanceOf(Array);
+        expect(
+          pizza.ingredients.every((i: unknown) => typeof i === 'string')
+        ).toBe(true);
+        expect(pizza).toHaveProperty('price');
+        expect(pizza.price).toEqual(expect.any(Number));
+        expect(pizza).toHaveProperty('pic_url');
+        expect(
+          typeof pizza.pic_url === 'string' || pizza.pic_url === null
+        ).toBe(true);
+      });
     });
   });
 
